f7c53420e770528da51758d740390b76d8af945c
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2024-05-18 21:40:10 +0600

    Update Scaling.cs

--- Assets/Scripts/Scaling.cs
+++ Assets/Scripts/Scaling.cs
@@ -4,7 +4,7 @@ public class Scaling : MonoBehaviour
 {
     [SerializeField] private float _scalingSpeed;
 
-    private void Update()
+    private void Update()z
     {
         Vector3 deltaScale = Vector3.one * _scalingSpeed * Time.deltaTime;
 
