ee0590cded0e1f9998c9e096d4b6ada8e6fbeeeb
Fletch17 <89846786+Fletch17@users.noreply.github.com>
2024-05-18 22:18:28 +0600

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,12 +2,12 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] private float _movementSpeed;     
+    [SerializeField] private float _movementSpeed;
 
     private void Update()
     {
-        var movementDirection = Vector3.forward.normalized * _movementSpeed;
-        
+        var movementDirection = Vector3.forward * _movementSpeed;
+
         transform.Translate(movementDirection, Space.Self);
     }
 }
